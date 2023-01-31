5c1996e82de1702c4ebf9fa316b062016eb500bd
Jesse Page <jesse.page@corebts.com>
2023-01-31 16:25:46 -0600

    project completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from "react"
+import Header from './components/Header'
+import Meme from './components/Meme'
 
-function App() {
+export default function App() {
 
   return (
-    <div className="App">
-      
+    <div className="body">
+      <Header />
+      <Meme />
     </div>
   )
 }
 
-export default App
+
